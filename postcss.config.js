21f22f1147b7b1f21baf2dc3ac9736db71aae3da
Alexander May <alexander.may@kratzer-automation.com>
2020-03-03 14:56:09 +0100

    fix build

--- postcss.config.js
+++ postcss.config.js
@@ -9,13 +9,19 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   whitelistPatterns: [/fa/, /fal/, /fas/],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
 });
 
 module.exports = {
   plugins: [
     require("tailwindcss"),
-    require("postcss-preset-env"),
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require("postcss-preset-env")({
+      stage: 1,
+      features: {
+        "focus-within-pseudo-class": false
+      }
+    }),
     ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 };
